d85bae77219c9333c15cc5cca5e8fe56f17891aa
Cody Hartman <codybot@gmail.com>
2024-02-11 20:04:16 -0700

    add scan for json

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -1,89 +1,138 @@
 package scan
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Layer struct {
-	id       string
-	severity string
+	id       string `json:"id"`
+	severity string `json:"severity"`
 }
 
 type ImageScanInfo struct {
-	Name   string
-	Layers []Layer
+	Name   string  `json:"name"`
+	Layers []Layer `json:"layers"`
 }
 
 type Metrics struct {
-	baseScore           float64
-	impactScore         float64
-	exploitabilityScore float64
+	baseScore           float64 `json:"baseScore"`
+	impactScore         float64 `json:"impactScore"`
+	exploitabilityScore float64 `json:"exploitabilityScore"`
 }
 
 type Cvss struct {
-	source         string
-	type_          string
-	version        string
-	vector         string
-	metrics        Metrics
-	vendorMetadata interface{}
+	source         string      `json:"source"`
+	type_          string      `json:"type"`
+	version        string      `json:"version"`
+	vector         string      `json:"vector"`
+	metrics        Metrics     `json:"metrics"`
+	vendorMetadata interface{} `json:"vendorMetadata"`
 }
 type Fix struct {
-	versions []string
-	state    string
+	versions []string `json:"versions"`
+	state    string   `json:"state"`
 }
 type Vulnerability struct {
-	id          string
-	dataSource  string
-	namespace   string
-	severity    string
-	urls        []string
-	description string
-	cvss        []string
-	advistories []string
+	id          string   `json:"id"`
+	dataSource  string   `json:"dataSource"`
+	namespace   string   `json:"namespace"`
+	severity    string   `json:"severity"`
+	urls        []string `json:"urls"`
+	description string   `json:"description"`
+	cvss        []string `json:"cvss"`
+	advisories  []string `json:"advisories"`
 }
 type Package struct {
-	name    string
-	version string
+	name    string `json:"name"`
+	version string `json:"version"`
 }
 type MatchDetails struct {
-	type_      string
-	matcher    string
-	searchedBy interface{}
-	found      interface{}
-	namespace  string
-	package_   Package
+	type_      string      `json:"type"`
+	matcher    string      `json:"matcher"`
+	searchedBy interface{} `json:"searchedBy"`
+	found      interface{} `json:"found"`
+	namespace  string      `json:"namespace"`
+	package_   Package     `json:"package"`
 }
 
 type ArtifactLocation struct {
-	path    string
-	layerID string
+	path    string `json:"path"`
+	layerID string `json:"layerId"`
 }
 
 type Artifact struct {
-	id       string
-	name     string
-	version  string
-	type_    string
-	location ArtifactLocation
+	id       string           `json:"id"`
+	name     string           `json:"name"`
+	version  string           `json:"version"`
+	type_    string           `json:"type"`
+	location ArtifactLocation `json:"location"`
 }
 
 type Match struct {
-	Vulnerability          Vulnerability
-	RelatedVulnerabilities []Vulnerability
-	Description            string
-	Cvss                   Cvss
-	Fix                    Fix
-	MatchDetails           MatchDetails
-	Artifact               Artifact
+	Vulnerability          Vulnerability   `json:"vulnerability"`
+	RelatedVulnerabilities []Vulnerability `json:"relatedVulnerabilities"`
+	Description            string          `json:"description"`
+	Cvss                   Cvss            `json:"cvss"`
+	Fix                    Fix             `json:"fix"`
+	MatchDetails           MatchDetails    `json:"matchDetails"`
+	Artifact               Artifact        `json:"artifact"`
 }
 
 type ScanInfo struct {
-	Matches []ImageScanInfo
+	Matches []ImageScanInfo `json:"matches"`
 }
 type Entry struct {
+	Match Match
+	Error error
 }
 
 type Stream struct {
-	Stream chan Entry
+	stream chan Entry
 }
 
-func LoadScan(path string) {
-	return
+func NewJSONStream() Stream {
+	return Stream{
+		stream: make(chan Entry),
+	}
+}
+
+func (s Stream) LoadScan(path string) {
+	defer close(s.stream)
+
+	// Open file to read.
+	file, err := os.Open(path)
+	if err != nil {
+		s.stream <- Entry{Error: fmt.Errorf("open file: %w", err)}
+		return
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	// Read opening delimiter. `[` or `{`
+	if _, err := decoder.Token(); err != nil {
+		s.stream <- Entry{Error: fmt.Errorf("decode opening delimiter: %w", err)}
+		return
+	}
+
+	// Read file content as long as there is something.
+	i := 1
+	for decoder.More() {
+		var match Match
+		if err := decoder.Decode(&match); err != nil {
+			s.stream <- Entry{Error: fmt.Errorf("decode line %d: %w", i, err)}
+			return
+		}
+		s.stream <- Entry{Match: match}
+
+		i++
+	}
+
+	// Read closing delimiter. `]` or `}`
+	if _, err := decoder.Token(); err != nil {
+		s.stream <- Entry{Error: fmt.Errorf("decode closing delimiter: %w", err)}
+		return
+	}
 }
