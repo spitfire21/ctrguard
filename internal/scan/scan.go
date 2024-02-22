75867661d52bdf307595932a04d4425c17ed6de6
Cody <dev.ops@applievable.com>
2024-02-21 17:16:22 -0700

    fix counting

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -353,7 +353,7 @@ func BuildSBOMFindings(grype *GrypeFormat) *SBOMFindings {
 	findings := &SBOMFindings{}
 
 	for _, entry := range grype.Matches {
-		switch entry.Vulnerability.Severity {
+		switch strings.ToUpper(entry.Vulnerability.Severity) {
 		case "CRITICAL":
 			findings.NumCritical++
 		case "HIGH":
@@ -364,11 +364,8 @@ func BuildSBOMFindings(grype *GrypeFormat) *SBOMFindings {
 			findings.NumLow++
 		case "INFO":
 			findings.NumInfo++
-
 		}
-
 	}
-
 	return findings
 }
 func BuildSBOMFindingsPerID(grype *GrypeFormat) *map[string]SBOMFindings {
@@ -377,56 +374,172 @@ func BuildSBOMFindingsPerID(grype *GrypeFormat) *map[string]SBOMFindings {
 
 	for _, entry := range grype.Matches {
 		for _, artifact := range entry.Artifact.Locations {
-			if val, ok := findings[artifact.LayerID]; !ok {
+			if _, ok := findings[artifact.LayerID]; !ok {
 				findings[artifact.LayerID] = SBOMFindings{
 					NumCritical: 0,
 					NumHigh:     0,
 					NumMedium:   0,
 					NumLow:      0,
 					NumInfo:     0,
-				}
-				switch entry.Vulnerability.Severity {
-				case "CRITICAL":
-					val.NumCritical++
-				case "HIGH":
-					val.NumHigh++
-				case "MEDIUM":
-					val.NumMedium++
-				case "LOW":
-					val.NumLow++
-				case "INFO":
-					val.NumInfo++
-				}
 
-				findings[artifact.LayerID] = val
-			} else {
-				switch entry.Vulnerability.Severity {
-				case "CRITICAL":
-					val.NumCritical++
-				case "HIGH":
-					val.NumHigh++
-				case "MEDIUM":
-					val.NumMedium++
-				case "LOW":
-					val.NumLow++
-				case "INFO":
-					val.NumInfo++
-
+					Vulnerabilities: map[string]VulnerabilitySBOM{},
 				}
-
-				findings[artifact.LayerID] = val
 			}
+			if val, ok := findings[artifact.LayerID]; ok {
+				if _, ok := findings[artifact.LayerID].Vulnerabilities[entry.Vulnerability.ID]; !ok {
+					val.Vulnerabilities[entry.Vulnerability.ID] = VulnerabilitySBOM{
+						VulnID:    entry.Vulnerability.ID,
+						Severity:  entry.Vulnerability.Severity,
+						Locations: NewSet(),
+						Artifacts: NewSet(),
+					}
+					val.Vulnerabilities[entry.Vulnerability.ID].Locations.Add(artifact.Path)
+					val.Vulnerabilities[entry.Vulnerability.ID].Artifacts.Add(entry.Artifact.ID)
+					switch strings.ToUpper(entry.Vulnerability.Severity) {
+					case "CRITICAL":
+						val.NumCritical++
+					case "HIGH":
+						val.NumHigh++
+					case "MEDIUM":
+						val.NumMedium++
+					case "LOW":
+						val.NumLow++
+					case "INFO":
+						val.NumInfo++
+					}
+					findings[artifact.LayerID] = val
+				} else {
+					if val.Vulnerabilities[entry.Vulnerability.ID].Artifacts.Has(entry.Artifact.ID) {
+						break
+					}
+					val.Vulnerabilities[entry.Vulnerability.ID].Artifacts.Add(entry.Artifact.ID)
+					switch strings.ToUpper(entry.Vulnerability.Severity) {
+					case "CRITICAL":
+						val.NumCritical++
+					case "HIGH":
+						val.NumHigh++
+					case "MEDIUM":
+						val.NumMedium++
+					case "LOW":
+						val.NumLow++
+					case "INFO":
+						val.NumInfo++
+					}
+					val.Vulnerabilities[entry.Vulnerability.ID].Locations.Add(artifact.Path)
+					findings[artifact.LayerID] = val
+				}
+			}
+
 		}
-
 	}
-
 	return &findings
 }
 
+type VulnerabilitySBOM struct {
+	Locations *Set
+	Artifacts *Set
+	VulnID    string
+	Severity  string
+}
+
 type SBOMFindings struct {
-	NumCritical int
-	NumHigh     int
-	NumMedium   int
-	NumLow      int
-	NumInfo     int
+	NumCritical     int
+	NumHigh         int
+	NumMedium       int
+	NumLow          int
+	NumInfo         int
+	Vulnerabilities map[string]VulnerabilitySBOM
+}
+
+type Set struct {
+	list map[string]struct{} //empty structs occupy 0 memory
+}
+
+func (s *Set) Has(v string) bool {
+	_, ok := s.list[v]
+	return ok
+}
+
+func (s *Set) Add(v string) {
+	s.list[v] = struct{}{}
+}
+
+func (s *Set) Remove(v string) {
+	delete(s.list, v)
+}
+
+func (s *Set) Clear() {
+	s.list = make(map[string]struct{})
+}
+
+func (s *Set) Size() int {
+	return len(s.list)
+}
+
+func NewSet() *Set {
+	s := &Set{}
+	s.list = make(map[string]struct{})
+	return s
+}
+
+//optional functionalities
+
+// AddMulti Add multiple values in the set
+func (s *Set) AddMulti(list ...string) {
+	for _, v := range list {
+		s.Add(v)
+	}
+}
+
+type FilterFunc func(v string) bool
+
+// Filter returns a subset, that contains only the values that satisfies the given predicate P
+func (s *Set) Filter(P FilterFunc) *Set {
+	res := NewSet()
+	for v := range s.list {
+		if P(v) == false {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+func (s *Set) Union(s2 *Set) *Set {
+	res := NewSet()
+	for v := range s.list {
+		res.Add(v)
+	}
+
+	for v := range s2.list {
+		res.Add(v)
+	}
+	return res
+}
+
+func (s *Set) Intersect(s2 *Set) *Set {
+	res := NewSet()
+	for v := range s.list {
+		if s2.Has(v) == false {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+// Difference returns the subset from s, that doesn't exists in s2 (param)
+func (s *Set) Difference(s2 *Set) *Set {
+	res := NewSet()
+	for v := range s.list {
+		if s2.Has(v) {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+func (s *Set) ToString() string {
+	return fmt.Sprintf("%v", s.list)
 }
--- internal/scan/scan_test.go
+++ internal/scan/scan_test.go
@@ -35,3 +35,45 @@ func TestNewLoadScan(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestSBOMFindings(t *testing.T) {
+	grype := UnmarshalScan("../../examples/debian_11.json")
+	sbom := BuildSBOMFindings(&grype)
+	if sbom.NumCritical != 2 {
+		t.Fatalf("ERROR, num of critical is off %d", sbom.NumCritical)
+	}
+	if sbom.NumHigh != 12 {
+		t.Fatalf("ERROR, num of high is off %d", sbom.NumHigh)
+	}
+	if sbom.NumInfo != 0 {
+		t.Fatalf("ERROR, num of info is off %d", sbom.NumInfo)
+	}
+	if sbom.NumLow != 7 {
+		t.Fatalf("ERROR, num of low is off %d", sbom.NumLow)
+	}
+	if sbom.NumMedium != 18 {
+		t.Fatalf("ERROR, num of medium is off %d", sbom.NumMedium)
+	}
+}
+
+func TestSBOMFindingsPerID(t *testing.T) {
+	grype := UnmarshalScan("../../examples/debian_11.json")
+	sbom := BuildSBOMFindingsPerID(&grype)
+	for key, val := range *sbom {
+		if val.NumCritical != 2 {
+			t.Fatalf("ERROR, num of critical is off %v, %s, %d", val, key, val.NumCritical)
+		}
+		if val.NumHigh != 12 {
+			t.Fatalf("ERROR, num of high is off %s, %d", key, val.NumHigh)
+		}
+		if val.NumInfo != 0 {
+			t.Fatalf("ERROR, num of info is off %s, %d", key, val.NumInfo)
+		}
+		if val.NumLow != 7 {
+			t.Fatalf("ERROR, num of low is off %s, %d", key, val.NumLow)
+		}
+		if val.NumMedium != 18 {
+			t.Fatalf("ERROR, num of medium is off %s, %d", key, val.NumMedium)
+		}
+	}
+}
