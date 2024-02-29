4059dd11ba2487d73ceb590cbddb7807105c2daf
Cody Hartman <codybot@gmail.com>
2024-02-28 19:38:49 -0700

    fix test

--- cmd/image_test.go
+++ cmd/image_test.go
@@ -5,17 +5,20 @@ import (
 )
 
 func TestFindRunningImage(t *testing.T) {
-	commands := map[string]string{
-		"docker run --rm -i -t alpine /bin/sh --login": "alpine",
-		"docker run --rm -i -t alpine /bin/sh":         "alpine",
-		"docker run --rm -i -t alpine":                 "alpine",
-		"docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock --name Grype anchore/grype:latest $(ImageName):$(ImageTag)": "anchore/grype:latest",
+	commands := map[string][]string{
+		"docker run --rm -i -t alpine /bin/sh --login": []string{"alpine"},
+		"docker run --rm -i -t alpine /bin/sh":         []string{"alpine"},
+		"docker run --rm -i -t alpine":                 []string{"alpine"},
+		"docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock --name Grype anchore/grype:latest $(ImageName):$(ImageTag)": []string{"anchore/grype", "latest"},
 	}
 
 	for k, v := range commands {
-		image := findRunningImage(k)
-		if image != v {
+		image, version := findRunningImage(k)
+		if image != v[0] {
 			t.Errorf("Expected %s, got %s", v, image)
+		}
+		if len(v) > 1 && version != v[1] {
+			t.Errorf("Expected %s, got %s", v, version)
 		}
 	}
 }
